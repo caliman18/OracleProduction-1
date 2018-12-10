0c635ba561478f39470acff9b660ebd739708b01
Harrison Paxton <harrisonpaxton18@gmail.com>
2018-12-09 21:04:00 -0500

    Added class diagram PNG

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-
+  
   public static void main(String[] args) {
 
     // Write one line of code to create an ArrayList of products
